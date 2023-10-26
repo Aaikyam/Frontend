a6c2c0001c209ccdb46900e4bd9cb563dd11a501
Wade <hhh@example.com>
2023-10-27 03:44:40 +0530

    Saman Font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/fonts.css"],
   theme: {
     extend: {
       fontFamily: {
         Tilt: ["Tilt Neon", "sans-serif"],
+        Rose: ["Red Rose", "sans-serif"],
+        SAMAN__: ["SAMAN__", "sans-serif"],
       },
     },
   },
   plugins: [
-
+    function ({ addBase, theme }) {
+      addBase({
+        "@font-face": {
+          fontFamily: "SAMAN__",
+          src: `url('/src/fonts/SAMAN___.TTF') format('truetype')`, // Adjust the path accordingly
+          fontWeight: "normal",
+          fontStyle: "normal",
+        },
+      });
+    },
   ],
-}
-
+};
